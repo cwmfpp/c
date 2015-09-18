a8dab3bd4f19ace5407a3deeae688abd8b489448
chenwenmin <n030@borqs>
2015-09-18 15:59:24 +0800

    shell脚本读取文件中的ip地址并允许访问此ip

---
+++ 06parseFile/ip.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+while read line
+do
+	echo $line
+	iptables -P INPUT DROP
+	iptables -P OUTPUT DROP
+	iptables -P FORWARD DROP
+	iptables -A INPUT -s $line -j ACCEPT
+	iptables -A INPUT -d $line -j ACCEPT
+	iptables -A OUTPUT -s $line -j ACCEPT
+	iptables -A OUTPUT -d $line -j ACCEPT
+	iptables -A FORWARD -s $line -j ACCEPT
+	iptables -A FORWARD -d $line -j ACCEPT
+	echo $line
+done < ip.txt 
+
